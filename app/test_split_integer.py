09f76c18cb8bb99b56d4290c0b86e0ae8cd2dc3c
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-04-17 20:22:54 +0300

    Append result to test_parts_should_be_sorted_when_they_are_not_equal

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,7 +14,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert sorted(split_integer(12, 3)) == split_integer(12, 3)
+    result = split_integer(12, 3)
+    assert sorted(result) == result
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
